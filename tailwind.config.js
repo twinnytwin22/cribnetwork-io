241072a8f18284c15ce418cdaadde7ee17495346
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-06-30 02:17:01 -0700

    get owners

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,19 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        'professor': ['professor', 'sans-serif'],
+        'owners': ['owners', 'sans-serif'],
+        'owners-wide': ['owners-wide', 'sans-serif'],
+        'owners-xwide': ['owners-xwide', 'sans-serif'],
+        'owners-xxwide': ['owners-xxwide', 'sans-serif'],
+        'groupie-gloss': ['groupie-gloss', 'san-serif'],
+        'groupie-regular': ['groupie-regular', 'san-serif'],
+        'bubblegum-pop-highlight': ['bubblegum-pop-highlight', 'san-serif'],
+        'bubblegum-pop-shadow': ['bubblegum-pop-shadow', 'san-serif'],
+        'bubblegum-pop-vanilla': ['bubblegum-pop-vanilla', 'san-serif']
+    
+      },
     },
   },
   plugins: [],
