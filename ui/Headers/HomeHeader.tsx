1ea40161b435e5c0dd8e607ab564d9c851d40052
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-07-24 10:33:36 -0700

    Update HomeHeader.tsx

--- ui/Headers/HomeHeader.tsx
+++ ui/Headers/HomeHeader.tsx
@@ -19,7 +19,7 @@ function HomeHeader() {
           <span className="text-xs bg-primary-600 rounded-full text-white px-4 py-1.5 mr-3">New</span> <span className="text-sm font-medium">Flowbite is out! See what's new</span>
           <svg className="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"></path></svg>
         </a>
-        <h1 className="mb-4 text-4xl font-normal tracking-normal  text-zinc-900 md:text-5xl lg:text-7xl dark:text-white max-w-screen-lg mx-auto font-[owners]">
+        <h1 className="mb-4 text-4xl font-normal tracking-normal  text-zinc-900 md:text-5xl lg:text-7xl dark:text-white max-w-screen-lg mx-auto font-owners">
           <span className="underline underline-offset-3 decoration-8 decoration-red-300">
             Innovating&nbsp;
           </span>
