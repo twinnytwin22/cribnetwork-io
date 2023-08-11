f3a870fe68bc2191480a8ecd7da748cf8ad798da
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-08-10 17:12:13 -0700

    test headers

--- next.config.js
+++ next.config.js
@@ -1,4 +1,34 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const securityHeaders = [
+    {
+      key: "Content-Security-Policy",
+      value: "your-content-security-policy",
+    },
+    {
+      key: "X-Frame-Options",
+      value: "SAMEORIGIN",
+    },
+    {
+      key: "X-Content-Type-Options",
+      value: "nosniff",
+    },
+    {
+      key: "Referrer-Policy",
+      value: "your-referrer-policy",
+    },
+    {
+      key: "Permissions-Policy",
+      value: "your-permissions-policy",
+    },
+  ];
+  
+  module.exports = {
+    async headers() {
+      return [
+        {
+          source: "/(.*)",
+          headers: securityHeaders,
+        },
+      ];
+    },
+  };
+  
