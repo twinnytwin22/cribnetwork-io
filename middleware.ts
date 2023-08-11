92c1604da428757278c50d1e4cd8867488a9d804
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-08-10 18:19:20 -0700

    testing CSP

--- middleware.ts
+++ middleware.ts
@@ -13,6 +13,11 @@ export async function middleware(req: NextRequest) {
   const {
     data: { session },
   } = await supabase.auth.getSession();
+  console.log(session);
+  res.headers.set(
+    "Content-Security-Policy",
+    `default-src 'self'; script-src 'self' 'nonce-${nonce}' cdnjs.cloudflare.com; style-src 'self' 'nonce-${nonce}' cdnjs.cloudflare.com; img-src 'self' data:; font-src 'self' cdnjs.cloudflare.com; connect-src 'self'; frame-src 'self'; object-src 'none'`
+  );
 
   // if (session) {
   //   return user;
