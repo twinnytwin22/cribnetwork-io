80a2eb790a9621f46596429d7af0820be9788b77
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-08-15 00:23:06 -0700

    Update page.tsx

--- app/portal/page.tsx
+++ app/portal/page.tsx
@@ -4,10 +4,13 @@ import { useAuthProvider } from '../context/auth'
 
 function page() {
     const { user, profile } = useAuthProvider()
-    return profile && (
+    return (
         <div className='relative place-items-center min-h-full mx-auto w-full p-8'>
-            <p className='font-bold text-xl text-center'>{`Hello, ${profile.full_name}`}</p>
-            <p className='text-xs text-center'>{user.email}&nbsp;|&nbsp;{profile.username}</p>
+            {profile &&
+                <React.Fragment>
+                    <p className='font-bold text-xl text-center'>{`Hello, ${profile.full_name}`}</p>
+                    <p className='text-xs text-center'>{user.email}&nbsp;|&nbsp;{profile.username}</p>
+                </React.Fragment>}
         </div>
     )
 }
