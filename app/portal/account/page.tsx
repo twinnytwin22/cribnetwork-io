27b11bef7941c89084a086ca288f062a986a912e
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-08-16 17:21:49 -0700

    Update page.tsx

--- app/portal/account/page.tsx
+++ app/portal/account/page.tsx
@@ -2,7 +2,7 @@
 import AccountForm from '@/ui/Sections/Account'
 import React, { useState } from 'react'
 import { BsFillQuestionSquareFill } from 'react-icons/bs'
-function page() {
+function AccountPage() {
     return (
         <section className='w-full h-full mx-auto relative'>
             <PortalPageTitle title='Account Settings' toolTip='Get ya mind right' />
@@ -13,7 +13,7 @@ function page() {
     )
 }
 
-export default page
+export default AccountPage
 
 export const PortalPageTitle = ({ title, toolTip, subtitle }: { title: string, subtitle?: string | null, toolTip?: string | null | boolean }) => {
     const [showTooltip, setShowTooltip] = useState(false);
