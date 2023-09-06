639d4db154d1bfc5656a33a06bf08971675abe11
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-09-06 01:50:51 -0700

    https

--- app/api/webhooks/courses-webhook/route.ts
+++ app/api/webhooks/courses-webhook/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server"
 export async function POST(req: Request) {
 if(req.method === 'POST') {
     const payload = req?.json()
-    
+    console.log(payload)
     NextResponse.json({ 'success': 'Sanity Webhook is working', 'status': 200})
 } else {
     NextResponse.json({'error': 'Not a POST, or working, please update and try again', 'status': 405})
