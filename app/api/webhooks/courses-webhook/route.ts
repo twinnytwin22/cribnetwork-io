1c4733b17861fbb9e20695c9ba2a00a0a52ef214
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-09-06 03:14:42 -0700

    Update route.ts

--- app/api/webhooks/courses-webhook/route.ts
+++ app/api/webhooks/courses-webhook/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import { isValidSignature, SIGNATURE_HEADER_NAME } from '@sanity/webhook'
 import { headers } from 'next/headers'
-
+import { revalidateTag } from 'next/cache'
 export async function POST(req: Request) {
   try {
     if (req.method === 'POST') {
@@ -10,12 +10,12 @@ export async function POST(req: Request) {
         return NextResponse.json({ error: 'Payload is empty', status: 406 })
       }
 
-      const signature = headers().get(SIGNATURE_HEADER_NAME) as string
+      const signature = headers().get[SIGNATURE_HEADER_NAME] as string
       if (!signature) {
         return NextResponse.json({ error: 'No Signature', status: 401 })
       }
 
-      const secret = process.env.SANITY_WEBHOOK_SECRET!
+      const secret = process.env.SANITY_WEBHOOK_SECRET?.trim()
       if (!secret) {
         return NextResponse.json({ error: 'Webhook secret not set', status: 401 })
       }
