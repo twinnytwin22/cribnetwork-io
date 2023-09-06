2bf0a591a93f150790e630a4bc031620318398c6
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-09-06 02:58:43 -0700

    Update route.ts

--- app/api/webhooks/courses-webhook/route.ts
+++ app/api/webhooks/courses-webhook/route.ts
@@ -2,26 +2,44 @@ import { NextResponse } from 'next/server'
 import { isValidSignature, SIGNATURE_HEADER_NAME } from '@sanity/webhook'
 import { headers } from 'next/headers'
 
-const secret = process.env.SANITY_WEBHOOK_SECRET! as string
+export async function POST(req: Request) {
+  try {
+    if (req.method === 'POST') {
+      const payload = await req?.text()
+      if (!payload) {
+        return NextResponse.json({ error: 'Payload is empty', status: 406 })
+      }
 
-export async function POST (req: Request) {
-  if (req.method === 'POST') {
-    const payload = await req?.text()
-    const signature = headers().get(SIGNATURE_HEADER_NAME) as string
-    
-    if (!isValidSignature(payload, signature, secret)) {
-      return NextResponse.json({ error: 'Invalid Signature', status: 401 })
+      const signature = headers().get(SIGNATURE_HEADER_NAME) as string
+      if (!signature) {
+        return NextResponse.json({ error: 'No Signature', status: 401 })
+      }
+
+      const secret = process.env.SANITY_WEBHOOK_SECRET!
+      if (!secret) {
+        return NextResponse.json({ error: 'Webhook secret not set', status: 401 })
+      }
+
+      if (!isValidSignature(payload, signature, secret)) {
+        return NextResponse.json({ error: 'Invalid Signature', status: 401 })
+      }
+
+      return NextResponse.json({
+        success: 'Sanity Webhook is working',
+        status: 200
+      })
+    } else {
+      return NextResponse.json({
+        error: 'Method not allowed, or working, please update and try again',
+        status: 405
+      })
     }
-
-    const Authorization = req.headers
+  } catch (error) {
+    // Handle any unexpected errors here
+    console.error('Error in webhook processing:', error)
     return NextResponse.json({
-      success: 'Sanity Webhook is working',
-      status: 200
-    })
-  } else {
-    return NextResponse.json({
-      error: 'Method not allowed, or working, please update and try again',
-      status: 405
+      error: 'Internal server error',
+      status: 500
     })
   }
 }
