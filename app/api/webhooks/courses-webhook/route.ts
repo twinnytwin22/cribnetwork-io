fdfaa62cc69c61b742b9d62ef0e91d6eacfaaee2
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-09-06 02:26:06 -0700

    testing webhook

--- app/api/webhooks/courses-webhook/route.ts
+++ app/api/webhooks/courses-webhook/route.ts
@@ -1,11 +1,25 @@
-import { NextResponse } from "next/server"
+import { NextResponse } from 'next/server'
+import { isValidSignature, SIGNATURE_HEADER_NAME } from '@sanity/webhook'
 
-export async function POST(req: Request) {
-if(req.method === 'POST') {
-    const payload = req?.json()
-    console.log(payload)
-    NextResponse.json({ 'success': 'Sanity Webhook is working', 'status': 200})
-} else {
-    NextResponse.json({'error': 'Not a POST, or working, please update and try again', 'status': 405})
-}
+const secret = process.env.SANITY_WEBHOOK_SECRET! as string
+
+export async function POST (req: Request) {
+  if (req.method === 'POST') {
+    const signature = req.headers.get(SIGNATURE_HEADER_NAME)
+    const payload = await req?.json()
+    if (!isValidSignature(payload, signature!, secret)) {
+      return NextResponse.json({ error: 'Invalid Signature', status: 401 })
+    }
+
+    const Authorization = req.headers
+    return NextResponse.json({
+      success: 'Sanity Webhook is working',
+      status: 200
+    })
+  } else {
+    return NextResponse.json({
+      error: 'Method not allowed, or working, please update and try again',
+      status: 405
+    })
+  }
 }
