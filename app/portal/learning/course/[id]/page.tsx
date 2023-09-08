7c063f0d94381c10fb575a352459854b7e63a1d5
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-09-08 02:24:30 -0700

    Update page.tsx

--- app/portal/learning/course/[id]/page.tsx
+++ app/portal/learning/course/[id]/page.tsx
@@ -28,7 +28,7 @@ export default async function Page ({ params }: { params: { id: string } }) {
               <div className='bg-white dark:bg-black rounded-lg p-8 border border-zinc-200 dark:border-zinc-800 space-y-4'>
                 <h1 className='text-xl font-bold text-black dark:text-white'>Course Lessons</h1>
                 {course?.lessons.map((lesson: any) => (
-                  <React.Fragment>
+                  <React.Fragment key={lesson?.title}>
                                   <div className='border-t border-zinc-300 dark:border-zinc-800'/>
 
                 <p className='text-zinc-800 dark:text-zinc-300 font-semibold text-lg'>{lesson?.title}</p>
