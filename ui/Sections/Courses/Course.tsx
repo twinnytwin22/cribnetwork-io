c7260677db76415a7d34965371434a57512dccb4
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-09-11 14:05:06 -0700

    Update Course.tsx

--- ui/Sections/Courses/Course.tsx
+++ ui/Sections/Courses/Course.tsx
@@ -11,13 +11,12 @@ function getYoutubeVideoId(url) {
 }
 
 function Course({ course, image }) {
-  const [activeLesson, setActiveLesson] = useState<any>(null);
+ // const [activeLesson, setActiveLesson] = useState<any>(null);
   const [activeLessonIndex, setActiveLessonIndex] = useState<number | any>(0);
   const [activeModuleIndex, setActiveModuleIndex] = useState<any>(null);
   const [finishedScreen, setFinishedScreen] = useState(false)
   const courseHeaderStyle = 'text-black dark:text-zinc-100 font-bold text-3xl'
   const courseBodyStyle = 'text-black dark:text-zinc-100  text-lg'
-
 
   function countModulesInCourse(course: { lessons: { modules: string | any[]; }[]; }) {
     let totalModules = 0;
@@ -50,10 +49,41 @@ function Course({ course, image }) {
     setActiveModuleIndex(moduleIndex);
   };
 
-  const moduleAndLessonCount = countModulesInCourse(course) + course.lessons.length;
   const lessons = course?.lessons || [];
   const modules = lessons[activeLessonIndex]?.modules || [];
+  const totalLessons = lessons.length;
 
+  const moduleAndLessonCount = countModulesInCourse(course) + lessons.length; 
+  let progressPercentage = 0;
+
+  if (totalLessons > 0) {
+    if (activeLessonIndex > 0) {
+      // If the active lesson is not the last lesson, calculate progress normally
+      progressPercentage = ((activeLessonIndex + 1) * 100) / totalLessons;
+    } else if (activeLessonIndex === totalLessons - 1 && !finishedScreen) {
+      // If the active lesson is the last lesson and not in the finished screen,
+      // set progress to 99.99% to avoid reaching 100% prematurely
+      progressPercentage = 99.99;
+    } else {
+      // If the active lesson is the last lesson and in the finished screen,
+      // set progress to 100%
+      progressPercentage = 90;
+    }
+  }
+
+  // Use a state variable to store and update the progress
+  const [progress, setProgress] = useState(progressPercentage);
+
+  // Update the progress whenever the active lesson changes
+  useEffect(() => {
+    const newProgress =
+     totalLessons > 0 ? ((activeLessonIndex + 1) * 100) / totalLessons : 0;
+    setProgress(newProgress);
+  }, [activeLessonIndex]);
+
+  // This totals the modules and lessons count
+
+console.log(moduleAndLessonCount)
   const handleNextClick = () => {
     if (activeModuleIndex === null) {
       // No module is active, try to go to the first module in the current lesson
@@ -101,9 +131,41 @@ function Course({ course, image }) {
   
   
 
+  const renderProgressBar = () => {
+    return (
+      <div>
+      <div className="relative pt-1">
+        <div className="flex mb-2 items-center justify-between">
+          <div>
+            <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full">
+              Lesson Progress
+            </span>
+          </div>
+          <div className="text-right">
+            <span className="text-xs font-semibold inline-block text-red-3">
+              {progress.toFixed(2)}%
+            </span>
+          </div>
+        </div>
+        <div 
+                    style={{ width: `${progress.toFixed(2)}%` }}
+
+        className="flex h-2 mb-4 overflow-hidden text-xs bg-red-300 rounded">
+          <div
+            style={{ width: `100%` }}
+            className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center border-zinc-800"
+          ></div>
+        </div>
+      </div>
+      {/* ... (other UI components) */}
+    </div>
+    )
+  }
+
   return (
     <section className='w-full h-full mx-auto relative max-w-screen flex pt-16 max-h-screen min-h-screen bg-zinc-100 dark:bg-zinc-950'>
-      <aside className='w-72 min-w-[288px] max-w-full bg-zinc-100 dark:bg-zinc-950 border-r border-zinc-300 dark:border-zinc-800 overflow-y-scroll relative z-20'>
+      <aside className='w-72 min-w-[288px] bg-zinc-100 dark:bg-zinc-950 border-r border-zinc-300 dark:border-zinc-800 overflow-hidden relative z-20 h-screen'>
+        <div className='fixed overflow-y-scroll w-72 h-screen'>
         <div className='w-full relative'>
           <Image
             className='relative bg-cover z-0 bg-center bg-no-repeat rounded-md overflow-hidden'
@@ -133,7 +195,7 @@ function Course({ course, image }) {
           {course?.lessons.map((lesson: any) => (
             <React.Fragment key={lesson?._id || lesson?.title}>
               <div
-                className={`border-t border-zinc-300 dark:border-zinc-800 flex ${activeLesson?.title === lesson?.title ? 'border-red-300' : ''
+                className={`border-t border-zinc-300 dark:border-zinc-800 flex ${lessons[activeLessonIndex]?.title  === lesson?.title ? 'border-red-300' : ''
                   }`}
                 onClick={() => handleLessonClick(lesson)}
                 style={{ cursor: 'pointer' }}
@@ -165,19 +227,21 @@ function Course({ course, image }) {
             </React.Fragment>
           ))}
         </div>
+        </div>
       </aside>
-      <div className='p-8 place-items-center max-w-screen w-full min-h-[80vh] h-full text-black dark:text-white'>
+      <div className='p-8 place-items-center max-w-screen w-full min-h-[100vh] h-full text-black dark:text-white'>
         {!finishedScreen && activeModuleIndex !== null ? (
           <div className='space-y-8'>
+            {renderProgressBar()}
           <h1 className={courseHeaderStyle}
             >{modules[activeModuleIndex]?.title}</h1>
             <p className='hidden'>{JSON.stringify(modules[activeModuleIndex].content)}</p>
             {modules[activeModuleIndex]?.videoUrl && (
               <iframe
-                className='mx-auto rounded border border-zinc-200 dark:border-zinc-800'
+              className='mx-auto rounded border border-zinc-200 dark:border-zinc-800 aspect-video max-w-4xl w-full'
                 title="Lesson Video"
-                width="560"
-                height="315"
+                //width="560"
+              //  height="315"
                 src={`https://www.youtube.com/embed/${getYoutubeVideoId(modules[activeModuleIndex]?.videoUrl)}`}
                 allowFullScreen
               ></iframe>
@@ -187,16 +251,18 @@ function Course({ course, image }) {
           </div>
         ) : activeLessonIndex < lessons.length ? (
           <div className='space-y-8'>
+            {renderProgressBar()}
+
             <h1 className={courseHeaderStyle}
             >{lessons[activeLessonIndex]?.title}</h1>
              {/* Replace with actual lesson content */}
             <p className='hidden'>{JSON.stringify(lessons[activeLessonIndex])}</p>
             {lessons[activeLessonIndex]?.videoUrl && (
               <iframe
-                className='mx-auto rounded border border-zinc-200 dark:border-zinc-800'
-                title="Lesson Video"
-                width="560"
-                height="315"
+              className='mx-auto rounded border border-zinc-200 dark:border-zinc-800 aspect-video max-w-4xl w-full'
+              title="Lesson Video"
+              //  width="560"
+               // height="315"
                 src={`https://www.youtube.com/embed/${getYoutubeVideoId(lessons[activeLessonIndex]?.videoUrl)}`}
                 allowFullScreen
               ></iframe>
@@ -208,14 +274,16 @@ function Course({ course, image }) {
         )}
         {finishedScreen &&
           <div className='p-8 place-items-center w-full min-h-[80vh] h-full'>
+            {renderProgressBar()}
+
             <h1 className={courseHeaderStyle}>
 
               FINISHED</h1>
           </div>}
-        {!finishedScreen ? <button onClick={handleNextClick} className='bg-red-300 text-white p-2 mt-4 rounded font-semibold'>
+        {!finishedScreen ? <button onClick={handleNextClick} className='bg-red-300 text-black p-2 mt-4 rounded font-bold px-6'>
           Next
         </button>
-          : <Link href={`/portal/learning/course/${course?._id}`} className='bg-red-300 text-white p-2 mt-4 rounded font-semibold'>
+          : <Link href={`/portal/learning/course/${course?._id}`} className='bg-red-300 text-black font-bold p-2 mt-4 rounded px-6'>
             Exit
           </Link>
         }
