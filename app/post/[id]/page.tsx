06722f558cf40aefe9f4b4c9db544f8e4b52ce01
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-09-13 14:00:34 -0700

    Update page.tsx

--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -2,6 +2,7 @@ import { getBlogPosts, imageBuilder } from '@/lib/providers/sanity/sanity'
 import { PortableBlogText } from '@/ui/Components/PortableBlogText'
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 export const revalidate = 0
 
 export async function generateStaticParams() {
@@ -41,9 +42,16 @@ async function Page({ params }: { params: { id?: string, slug: string } }) {
     }
 
     return (
-        <div className='h-screen items-center flex'>
-            <p>No valid blog post found.</p>
-        </div>
+      <section className="bg-white dark:bg-zinc-950 h-screen ">
+    <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 flex items-center h-full">
+        <div className="mx-auto max-w-screen-sm text-center">
+            <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">404</h1>
+            <p className="mb-4 text-3xl tracking-tight font-bold text-zinc-900 md:text-4xl dark:text-white">Something's missing.</p>
+            <p className="mb-4 text-lg font-light text-zinc-500 dark:text-zinc-400">Sorry, we can't find that page. You'll find lots to explore on the home page. </p>
+            <Link href="/" className="inline-flex text-white bg-primary-600 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4">Back to Homepage</Link>
+        </div>   
+    </div>
+</section>
     )
 }
 
