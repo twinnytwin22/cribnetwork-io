a08044e15b57ac618f2a2e0efefe87c7539d9898
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-09-13 16:55:55 -0700

    Update page.tsx

--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -20,7 +20,7 @@ export async function generateMetadata(
   parent: ResolvingMetadata
 ): Promise<Metadata> {
   // Read route params
-  const id = params.id;
+  const {id } = params;
 
   // Fetch data
   const { res, slugs } = await getBlogPosts();
@@ -37,7 +37,7 @@ export async function generateMetadata(
     return {
       title: relatedPost?.title,
       openGraph: {
-        images: [image!, ...previousImages],
+        images: [image!]//, ...previousImages],
       },
     };
   }
@@ -61,7 +61,7 @@ export async function generateStaticParams() {
   }
 
 
-async function Page({ params, searchParams }: { params: { id?: string, slug: string }, searchParams }) {
+async function Page({ params, searchParams }: Props) {
     const { success, slugs, res } = await getBlogPosts()
 
     if (success) {
@@ -93,8 +93,8 @@ async function Page({ params, searchParams }: { params: { id?: string, slug: str
     <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 flex items-center h-full">
         <div className="mx-auto max-w-screen-sm text-center">
             <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">404</h1>
-            <p className="mb-4 text-3xl tracking-tight font-bold text-zinc-900 md:text-4xl dark:text-white">Something's missing.</p>
-            <p className="mb-4 text-lg font-light text-zinc-500 dark:text-zinc-400">Sorry, we can't find that page. You'll find lots to explore on the home page. </p>
+            <p className="mb-4 text-3xl tracking-tight font-bold text-zinc-900 md:text-4xl dark:text-white">Something&apos;s missing.</p>
+            <p className="mb-4 text-lg font-light text-zinc-500 dark:text-zinc-400">Sorry, we can&apos;t find that page. You&apos;ll find lots to explore on the home page. </p>
             <Link href="/" className="inline-flex text-white bg-primary-600 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4">Back to Homepage</Link>
         </div>   
     </div>
