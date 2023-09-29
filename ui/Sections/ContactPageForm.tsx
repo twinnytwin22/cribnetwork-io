39662e34044d7b7f27114c2816e536efb84fcc3f
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-09-29 00:03:10 -0700

    Update ContactPageForm.tsx

--- ui/Sections/ContactPageForm.tsx
+++ ui/Sections/ContactPageForm.tsx
@@ -122,14 +122,14 @@ const ContactForm = ({ handleClose }) => {
         <div className="flex space-x-3">
           <button
             type="submit"
-            className="py-3 font-owners px-5 text-xs md:text-sm font-light text-center text-black bg-red-300 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 hover:scale-105"
+            className="py-3 font-owners px-5 text-xs tracking-wide md:text-sm font-semibold text-center text-black bg-red-300 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 hover:scale-105"
           >
             Send message
           </button>
           <Link href={bookingUrl}>
             <button
 
-              className="py-3 font-owners px-5 text-xs md:text-sm font-light text-center text-black bg-red-300 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 hover:scale-105"
+              className="py-3 font-owners px-5 tracking-wide text-xs md:text-sm font-semibold text-center text-black bg-red-300 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 hover:scale-105"
             >
               Schedule a Call            </button>
           </Link>
