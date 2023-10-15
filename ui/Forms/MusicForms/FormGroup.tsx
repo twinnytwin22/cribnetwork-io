3087a58e4902fb2602f45d2e0318d8a22bfd2c3c
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-10-15 13:56:43 -0700

    Update FormGroup.tsx

--- ui/Forms/MusicForms/FormGroup.tsx
+++ ui/Forms/MusicForms/FormGroup.tsx
@@ -3,78 +3,89 @@
 import React from 'react';
 import UploadSongForm from './UploadSong';
 import AddArtistForm from './AddArtist';
-import {useSearchParams } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
 import Link from 'next/link';
 import { useQuery } from '@tanstack/react-query';
 import { supabase } from '@/lib/site/constants';
 
 function FormGroup() {
-  const params = useSearchParams();
-  const mode = params.get('mode');
+    const params = useSearchParams();
+    const mode = params.get('mode');
 
-const getAllSongs = async() => {
-    return mode
-}
+    const getAllSongs = async () => {
+        try {
+            const songs = await fetch('https://cribmusic.xyz/api/v1/getAllArtists', {
+                method: 'GET',
+                cache: 'no-cache',
+                headers: {
+                    'Accept': 'application/json',
 
-  const {data: songs} = useQuery({
-    queryKey: ['data'],
-    queryFn: () => fetch('https://cribmusic.xyz/v1/getAllSongs')
-  })
+                 },
 
-  return (
-    <div className='max-w-5xl mx-auto'>
-      <div className="flex space-x-3 ">
-        <div className="text-sm font-medium text-center border-b dark:border-zinc-700 mx-8">
-          <ul className="flex flex-wrap -mb-px ">
-            <li className="mr-2">
-              <Link
-                href="/portal/crib-music/?mode=song"
-                aria-current={mode === 'song' ? 'page' : undefined}
+            })
+            //const data = songs.json()
+            return songs
+        } catch (error) {
+            throw error
+        }
+    }
 
-                className={`inline-block p-4 rounded-t-lg ${
-                  mode === 'song'
-                    ? 'text-red-300 border-b-2 border-red-400 dark:text-red-300 dark:border-red-300'
-                    : 'text-zinc-500 border-b-2 border-transparent hover:text-red-400 hover:border-red-100 dark:hover:text-zinc-300'
-                }`}
-              >
-                Upload Song
-              </Link>
-            </li>
-            <li className="mr-2">
-              <Link
-                href="/portal/crib-music/?mode=artist"
-                className={`inline-block p-4 rounded-t-lg ${
-                  mode === 'artist'
-                  ? 'text-red-300 border-b-2 border-red-400 dark:text-red-300 dark:border-red-300'
-                  : 'text-zinc-500 border-b-2 border-transparent hover:text-red-400 hover:border-red-100 dark:hover:text-zinc-300'
-                }`}
-                aria-current={mode === 'artist' ? 'page' : undefined}
-              >
-                Add Artist
-              </Link>
-            </li>
-            <li className="mr-2">
-              <Link
-                href="/portal/crib-music/?mode=data"
-                className={`inline-block p-4 rounded-t-lg ${
-                  mode === 'data'
-                  ? 'text-red-300 border-b-2 border-red-400 dark:text-red-300 dark:border-red-300'
-                  : 'text-zinc-500 border-b-2 border-transparent hover:text-red-400 hover:border-red-100 dark:hover:text-zinc-300'
-                }`}
-                aria-current={mode === 'data' ? 'page' : undefined}
-              >
-                Data
-              </Link>
-            </li>
-          </ul>
+    const { data: songs } = useQuery({
+        queryKey: ['data'],
+        queryFn: () => getAllSongs()
+    })
+
+    return (
+        <div className='max-w-5xl mx-auto'>
+            <div className="flex space-x-3 ">
+                <div className="text-sm font-medium text-center border-b dark:border-zinc-700 mx-8">
+                    <ul className="flex flex-wrap -mb-px ">
+                        <li className="mr-2">
+                            <Link
+                                href="/portal/crib-music/?mode=song"
+                                aria-current={mode === 'song' ? 'page' : undefined}
+
+                                className={`inline-block p-4 rounded-t-lg ${mode === 'song'
+                                    ? 'text-red-300 border-b-2 border-red-400 dark:text-red-300 dark:border-red-300'
+                                    : 'text-zinc-500 border-b-2 border-transparent hover:text-red-400 hover:border-red-100 dark:hover:text-zinc-300'
+                                    }`}
+                            >
+                                Upload Song
+                            </Link>
+                        </li>
+                        <li className="mr-2">
+                            <Link
+                                href="/portal/crib-music/?mode=artist"
+                                className={`inline-block p-4 rounded-t-lg ${mode === 'artist'
+                                    ? 'text-red-300 border-b-2 border-red-400 dark:text-red-300 dark:border-red-300'
+                                    : 'text-zinc-500 border-b-2 border-transparent hover:text-red-400 hover:border-red-100 dark:hover:text-zinc-300'
+                                    }`}
+                                aria-current={mode === 'artist' ? 'page' : undefined}
+                            >
+                                Add Artist
+                            </Link>
+                        </li>
+                        <li className="mr-2">
+                            <Link
+                                href="/portal/crib-music/?mode=data"
+                                className={`inline-block p-4 rounded-t-lg ${mode === 'data'
+                                    ? 'text-red-300 border-b-2 border-red-400 dark:text-red-300 dark:border-red-300'
+                                    : 'text-zinc-500 border-b-2 border-transparent hover:text-red-400 hover:border-red-100 dark:hover:text-zinc-300'
+                                    }`}
+                                aria-current={mode === 'data' ? 'page' : undefined}
+                            >
+                                Data
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            {mode === 'song' && <UploadSongForm />}
+            {mode === 'artist' && <AddArtistForm />}
+            {mode === 'data' && <div> {JSON.stringify(songs)}</div>}
         </div>
-      </div>
-
-      {mode === 'song' && <UploadSongForm />}
-      {mode === 'artist' && <AddArtistForm />}
-      {mode === 'data' && <div> {JSON.stringify(songs)}</div>}
-    </div>
-  );
+    );
 }
 
 export default FormGroup;
