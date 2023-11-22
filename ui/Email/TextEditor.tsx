5c5b0bee41944ad0dbe1a4e0609f88ec63986c60
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-11-21 22:09:17 -0700

    update

--- ui/Email/TextEditor.tsx
+++ ui/Email/TextEditor.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { supabaseAdmin } from "@/lib/providers/supabase/supabase-lib-admin";
 import { useQuery } from "@tanstack/react-query";
 import { Editor } from "@tinymce/tinymce-react";
 import { useTheme } from "next-themes";
@@ -7,8 +6,8 @@ import { UseThemeProps } from "next-themes/dist/types";
 import { useEffect, useRef, useState } from "react";
 import { FaWindowClose } from "react-icons/fa";
 import { FaFile } from "react-icons/fa6";
-import { toast } from "react-toastify";
 import { Editor as TinyMCEEditor } from "tinymce";
+import { forceRerender, getExistingDocs, log, saveToDB, setNewContent } from "./actions";
 import { HTML, getEditorProps } from "./lib";
 import { useEditorStore } from "./store";
 
@@ -22,82 +21,43 @@ export default function TextEditor() {
   const { savedContent, setSavedContent } = useEditorStore();
   const [editorProps, setEditorProps] = useState<any>({});
   const [fileManagerOpen, setFileManagerOpen] = useState(false);
-  const getExistingDocs = async () => {
-    const { data } = await supabaseAdmin.from("email_templates").select();
+  useEffect(() => setMounted(true), []);
 
-    if (data) {
-      return data;
-    }
-  };
   const { data: existingDocs } = useQuery({
     queryKey: ["data"],
     queryFn: () => getExistingDocs(),
     enabled: mounted,
   });
 
-  // console.log(data, 'EXD')
-  const saveToDB = async () => {
-    try {
-      if (savedContent && editorRef.current) {
-        const { data, error } = await supabaseAdmin
-          .from("email_templates")
-          .upsert({
-            html: savedContent, //editorRef.current.getContent()
-          })
-          .select()
-          .single();
-        if (data) {
-          toast.success("File saved to db");
-        }
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  const forceRerender = async () => {
-    setMounted(false);
-    requestAnimationFrame(() => setMounted(true));
-  };
-
-  useEffect(() => setMounted(true), []);
-  // console.log(currentTheme, editorProps)
   useEffect(() => {
     setEditorProps(getEditorProps(currentTheme!));
-    forceRerender();
+    forceRerender(setMounted);
   }, [currentTheme!!, mounted, editorProps]);
 
-  const log = () => {
-    if (editorRef.current) {
-      setSavedContent(editorRef.current.getContent());
-      if (savedContent) {
-        toast.success("Saved");
-      }
-    }
-    // console.log(savedContent)
-  };
 
-  const setNewContent = (e) => {
-    const { value } = e.target;
-    if (value) {
-    console.log(value, "VALUE");
-    const option =  existingDocs!.find(({ title }) => title === value);//HTML.find(({ title }) => title === value) ||
-    console.log(savedContent);
-    // Reset the editor content with the new content
-    if (editorRef.current) {
-      editorRef.current.setContent(option?.element!);
-    }
-  }
-  };
   if (!mounted) {
     return null;
   }
-
 
   return (
     <div className="max-w-5xl mx-auto ">
       <div>
         {mounted && (
           <div className=" grayscale">
+            <div className="group py-2">
+              <input
+                className="block py-1.5 px-0 w-72 text-sm text-zinc-900 bg-transparent border-0 border-b-2 border-zinc-300 appearance-none dark:text-white dark:border-zinc-600 dark:focus:border-red-200 focus:outline-none focus:ring-0 focus:border-red-300 peer"
+                name="title"
+                id="title"
+                type="text"
+              />
+              <label
+                className="peer-focus:font-medium absolute text-sm text-zinc-500 dark:text-zinc-300 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-red-300 peer-focus:dark:text-red-200 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                htmlFor="title"
+              >
+                Document Name
+              </label>
+            </div>
             <Component
               onEditorChange={setSavedContent}
               //tinymceScriptSrc="/tinymce/tinymce.min.js"
@@ -109,13 +69,13 @@ export default function TextEditor() {
         <div className="flex items-center space-x-4 justify-center mx-auto py-4">
           <button
             className="dark:text-black font-work-sans text-white bg-black hover:bg-zinc-800 focus:ring-4 focus:outline-none focus:ring-zinc-300 font-medium rounded text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-white dark:hover:bg-zinc-200 dark:focus:ring-zinc-800 ease-in-out duration-300"
-            onClick={log}
+            onClick={() => log(editorRef, savedContent, setSavedContent)}
           >
             Log editor content
           </button>
           <button
             className="dark:text-black font-work-sans text-white bg-black hover:bg-zinc-800 focus:ring-4 focus:outline-none focus:ring-zinc-300 font-medium rounded text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-white dark:hover:bg-zinc-200 dark:focus:ring-zinc-800 ease-in-out duration-300"
-            onClick={saveToDB}
+            onClick={() => saveToDB(editorRef, savedContent)}
             //name="submitbtn"
             type="button"
           >
@@ -144,7 +104,7 @@ export default function TextEditor() {
               <button
                 value={option.title}
                 className="dark:text-black mx-auto font-work-sans text-white bg-black hover:bg-zinc-800 focus:ring-4 focus:outline-none focus:ring-zinc-300 font-medium rounded text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-white dark:hover:bg-zinc-200 dark:focus:ring-zinc-800 ease-in-out duration-300"
-                onClick={setNewContent}
+                onClick={(e) =>setNewContent(e, existingDocs, editorRef)}
               >
                 Use
               </button>
@@ -160,9 +120,13 @@ export default function TextEditor() {
             </div>
             {existingDocs?.map((doc) => (
               <div className="relative p-4" key={doc.id}>
-                <button value={doc.title} onClick={setNewContent} className="text-sm">
-                  <FaFile className="text-4xl w-14 "/>
-                {doc?.title}
+                <button
+                  value={doc.title}
+                  onClick={(e) => setNewContent(e, existingDocs, editorRef)}
+                  className="text-sm relative"
+                >
+                  <FaFile className="text-4xl w-14 " />
+                  {doc?.title}
                 </button>
               </div>
             ))}
---
+++ ui/Email/actions.ts
@@ -0,0 +1,70 @@
+import { supabaseAdmin } from "@/lib/providers/supabase/supabase-lib-admin";
+import { toast } from "react-toastify";
+import { HTML } from "./lib";
+
+export const getExistingDocs = async () => {
+    const { data } = await supabaseAdmin.from("email_templates").select();
+    if (data) {
+      return data;
+    }
+  };
+ export const forceRerender = async (setMounted: any) => {
+    setMounted(false);
+    requestAnimationFrame(() => setMounted(true));
+  };
+
+
+export const saveToDB = async (editorRef: any, savedContent: string,id?: any) => {
+    try {
+      if (savedContent && editorRef.current && !id) {
+        const { data, error } = await supabaseAdmin
+          .from("email_templates")
+          .upsert({
+            html: savedContent, //editorRef.current.getContent()
+          })
+          .select()
+          .single();
+        if (data) {
+          toast.success("File saved to db");
+        }
+      } else if (savedContent && editorRef && id!!) {
+        const { data, error } = await supabaseAdmin
+          .from("email_templates")
+          .upsert({
+            html: savedContent, //editorRef.current.getContent()
+          })
+          .eq("id", id)
+          .select()
+          .single();
+        if (data) {
+          toast.success("File saved to db");
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+    }
+  };
+
+
+export const log = (editorRef, savedContent, setSavedContent) => {
+    if (editorRef.current) {
+      setSavedContent(editorRef.current.getContent());
+      if (savedContent) {
+        toast.success("Saved");
+      }
+    }
+    // console.log(savedContent)
+  };
+  
+  export const setNewContent = (e, existingDocs, editorRef) => {
+    const { value } = e.target;
+    if (value && existingDocs) {
+      const option = [...HTML, ...existingDocs!].find(
+        ({ title }) => title === value,
+      );
+      if (editorRef.current) {
+        editorRef.current.setContent(option?.element!);
+      }
+    }
+  };
