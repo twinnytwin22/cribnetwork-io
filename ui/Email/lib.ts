5d2eeb7e2122473499f6142197729709409b8e0a
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-12-24 09:22:37 -0700

    Update lib.ts

--- ui/Email/lib.ts
+++ ui/Email/lib.ts
@@ -44,7 +44,7 @@ export const editorPropsDark = {
   init: {
     height: 600,
     plugins:
-      "save ai tinycomments mentions anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed permanentpen footnotes advtemplate advtable advcode editimage tableofcontents mergetags powerpaste tinymcespellchecker autocorrect a11ychecker typography inlinecss",
+    "save anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount",
     toolbar:
       "save | undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | align lineheight | tinycomments | checklist numlist bullist indent outdent | emoticons charmap | removeformat",
     tinycomments_mode: "embedded",
